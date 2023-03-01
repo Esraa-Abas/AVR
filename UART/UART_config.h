1cc8b30a6a4ee2cbcf5ec01d241f8142c4c6e3a1
Esraa Abas <96321035+Esraa-Abas@users.noreply.github.com>
2023-03-01 18:37:04 +0200

    Add files via upload

---
+++ UART/UART_config.h
@@ -0,0 +1,28 @@
+/*
+ * UART_config.h
+ *
+ *  Created on: Jan 26, 2023
+ *      Author: esraa
+ */
+
+#ifndef UART_CONFIG_H_
+#define UART_CONFIG_H_
+
+
+
+
+#define MODE               ASYNC
+#define PARITY             Disabled
+#define STOP_BIT           one_bit
+#define DATA_SIZE          eight_bits
+#define CLOCK_PARITY       rising_falling
+
+
+#define SPEED              single_speed
+#define F_SYS              freq_8m
+#define BUAD_RATE          rate_9600_pbs
+
+
+
+
+#endif /* UART_CONFIG_H_ */
